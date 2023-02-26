e0cf252f195c9edb3e8a1d6a4bef86b42b297ddf
Cellmer <michal.cellmer@gmail.com>
2023-02-26 17:52:07 +0100

    set boundaries to player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private float verticalInput;
     private float speed = 4.0f;
+    private float topBound = 6.0f;
+    private float bottomBound = -6.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,16 @@ public class PlayerController : MonoBehaviour
     {
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+
+        // boundaries
+        if(transform.position.y > topBound)
+        {
+            transform.position = new Vector3(-12, topBound, 0);
+        }
+
+        if (transform.position.y < bottomBound)
+        {
+            transform.position = new Vector3(-12, bottomBound, 0);
+        }
     }
 }
